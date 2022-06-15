edce26106445e75d22838b9bd227186df89ca422
TigerBig <jizlin575@gmail.com>
2022-06-14 18:09:19 -0700

    added updated and deleted operation for mongoDB

--- mongoDB/embedding.js
+++ mongoDB/embedding.js
@@ -1,44 +1,57 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/firstDB')
-    .then(() => console.log('Connected to MongoDB.'))
-    .catch(err => console.error('could not connect to MongoDB.', err)
-    );
+  .then(() => console.log('Connected to MongoDB.'))
+  .catch(err => console.error('could not connect to MongoDB.', err)
+  );
 
 const briefSchema = new mongoose.Schema({
-    name: String,
-    date: String,
-    done: Boolean
+  name: String,
+  date: String,
+  done: Boolean
 });
 const detailSchema = new mongoose.Schema({
-    description: String,
-    answer: String
+  description: String,
+  answer: String
 });
 const Brief = mongoose.model('Brief', briefSchema);
 const Detail = mongoose.model('Detail', detailSchema);
 const Question = mongoose.model('Question', new mongoose.Schema({
-    id: Number,
-    brief: briefSchema,
-    detail: detailSchema
+  id: Number,
+  brief: briefSchema,
+  detail: detailSchema
 }));
 
 async function createQuestion(id, brief, detail){
-    const question = new Question({
-        id,
-        brief,
-        detail
-    });
-    const result = await question.save();
-    console.log('question added:', result);
+  const question = new Question({
+    id,
+    brief,
+    detail
+  });
+  const result = await question.save();
+  console.log('question added:', result);
+  return result;
 }
 async function listQuestions() {
-    const questions = await Question
-        .find()
-        // .select('id', 'brief');
-    console.log(questions);
+  const questions = await Question
+    .find()
+  // .select('id', 'brief');
+  console.log(questions);
+}
+async function updateQuestion(questionID) {
+  const question = await Question.update({ _id: questionID}, {
+    $set: {
+      'detail.answer': 'No after update',
+      'brief.name': 'sum up updated'
+    }
+  });
 }
 
-createQuestion(3,
-  new Brief({name:'sum up', date:'Wed', done:true}),
-  new Detail({description: 'sum up two number', answer: 'none.'})
+async function run(){
+  const question = await createQuestion(3,
+    new Brief({name:'sum up', date:'Wed', done:true}),
+    new Detail({description: 'sum up two number', answer: 'none.'})
   );
-listQuestions();
+  await updateQuestion(question._id);
+  listQuestions();
+}
+run();
--- mongoDB/reference.js
+++ mongoDB/reference.js
@@ -32,7 +32,7 @@ function creatBrief(name, date, done){
     done
   });
   const result = brief.save();
-  console.log('Brief ', result);
+  // console.log('Brief ', result);
   return brief;
 }
 function createDetail(description, answer){
@@ -41,7 +41,7 @@ function createDetail(description, answer){
     answer
   });
   const result = detail.save();
-  console.log('Detail ', result);
+  // console.log('Detail ', result);
   return detail;
 }
 function createQuestion(id, brief, detail){
@@ -51,21 +51,57 @@ function createQuestion(id, brief, detail){
     detail
   });
   const result = question.save();
-  console.log('question added ', result);
+  // console.log('question added ', result);
 }
 function listQuestions() {
   const questions = Question
     .find()
-    .select('id', 'brief');
-  console.log(questions);
+    .populate('brief')
+    .select('id brief');
+  // console.log('list question: ', questions);
 }
-async function run(){
-  // const brief = await creatBrief('sum up', 'Wed', true);
-  // const detail = await createDetail('sum up two number', 'none.');
-  // createQuestion(6, brief, detail);
+async function updateBrief(brief) {
+  const result = await brief.set({
+    'name': 'sum updated'
+  });
+  await result.save();
+  console.log('update brief ', result);
+}
+async function updateDetail(id) {
+  // detail.set({
+  //   'answer': 'no even updated'
+  // });
+  // detail.answer = 'no even updated';
+  const result = await Detail.updateOne({id: id}, {
+    $set: {
+      answer: 'no even updated'
+    }
+  });
+  // await detail.save();
+  console.log('update detail: ', result);
+}
+async function initialize(){
+  console.log('Initializing...')
+  const brief = await creatBrief('sum up', 'Wed', true);
+  const detail = await createDetail('sum up two number', 'none.');
+  await createQuestion(6, brief, detail);
 
-  const brief = await Brief.findOne({name: 'sum up'});
-  const detail = await Detail.findOne({description: /.*sum.*/});
-  createQuestion(8, brief, detail._id);
+  // const brief = await Brief.findOne({name: 'sum up'});
+  // const detail = await Detail.findOne({description: /.*sum.*/});
+  // createQuestion(8, brief, detail._id);
+  // listQuestions();
+}
+async function update(){
+  // console.log('update here', Detail.findOne({id: 6}));
+  // const detail = Detail.findOne({id: 6});
+  // const brief = Brief.findOne({id: 6});
+  console.log('Updatding...')
+  updateDetail(6);
+  updateBrief(Brief.findOne({id: 6}));
+}
+function run(){
+  initialize();
+  update();
+  listQuestions();
 }
 run();
